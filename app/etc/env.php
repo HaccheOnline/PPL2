291673f6ff3e11e1f023b81cb0e787eb936f2c4a
Blugamma <blugammadesign@gmail.com>
2018-02-02 15:51:37 +0000

    Added Magento 2 App Folder

---
+++ app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'site_admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '4b4af270951b59610bab018b4bfe3629',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'haccheltdcom',
+        'username' => 'hacche69Oz',
+        'password' => 'Gaesh2ea',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 11 Jan 2018 11:58:47 +0000',
+  ),
+);

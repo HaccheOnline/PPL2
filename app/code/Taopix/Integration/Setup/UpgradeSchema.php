ac54f677b576a68d5d7babf5a23f56f03a2e9075
Oliver Smith <OliverSmith@Meld.local>
2018-02-09 15:23:47 +0000

    Edit UpgradeSchema.php

--- app/code/Taopix/Integration/Setup/UpgradeSchema.php
+++ app/code/Taopix/Integration/Setup/UpgradeSchema.php
@@ -7,19 +7,33 @@ namespace Taopix\Integration\Setup;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-class UpgradeSchema implements UpgradeSchemaInterface{
- public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context){
- if (version_compare($context->getVersion(), '2.0.1') < 0) {
- $setup->startSetup();
- $setup->getConnection()->addColumn(
- $setup->getTable('quote-item'),
- 'taopix_batchref',
- ['type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
- 'length' => '120',
- 'nullable' => false,
- 'default' => 'NULL',
- 'comment' => 'Taopix Batch Reference']);
- $setup->endSetup();
- } } }
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $installer = $setup;
+
+        $installer->startSetup();
+        if (version_compare($context->getVersion(), '1.0.0', '<')) {
+          $installer->getConnection()->addColumn(
+                $installer->getTable('quote_item'),
+                'taopix_batchref',
+                [
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => 50,
+                    'nullable' => true,
+                    'comment' => 'Taopix Batch Reference'
+                ]
+            );
+        }
+        $installer->endSetup();
+    }
+}
 
  ?>
